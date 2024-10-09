98b2b3173200fc5d5a2c10e82a86ca413ffa9adf
Alex <fer>
2024-10-09 22:20:47 +0300

    55

--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class TaskTwo {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите число a: "  );
+            System.out.println("Введите строку a: "  );
             String a =scanner.nextLine();
-            System.out.println("Введите число b: "  );
+            System.out.println("Введите строку b: "  );
             String b =scanner.nextLine();
             String result = (a.equals(b))?("Строки идентичны"):("Строки неидентичны");
             System.out.println(result);
