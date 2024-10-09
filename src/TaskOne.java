475814d5ab888bcdd6f6e208d05f7fefbefef517
Alex <fer>
2024-10-09 22:19:36 +0300

    5

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class TaskOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число a: ");
+        System.out.println("Введите целое число a: ");
         int a = scanner.nextInt();
-        System.out.println("Введите число b: ");
+        System.out.println("Введите целое число b: ");
         int b = scanner.nextInt();
         String compare = (a > b) ? "a > b" : (a < b) ? "a < b" : "a = b";
         System.out.println(compare);
